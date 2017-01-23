f7401ee25932dbbdab2dceb01f7ea8c0249d2f29
Onintza <oarakama@gmail.com>
2017-01-23 18:45:35 +0100

    aplikazioa 20170123

--- inst/metaheuRapp/server.R
+++ inst/metaheuRapp/server.R
@@ -220,33 +220,7 @@ shinyServer(function(input, output) {
   ###Problema kodea#
 
   output$problemaKodea <- renderText({
-    if (input$Problema == "Travelling salesman problem") {
-      cmatrix <- matrix(runif(100), ncol = 10)
-      tsp <- tspProblem(cmatrix)
-      paste(tsp[1])
-    }
-
-    else if (input$Problema == "Closest String Problem") {
-      cmatrix <-
-        matrix(
-          data = c('a', 'a', 'a', 'a', 'a', 'a'),
-          ncol = 2,
-          byrow = TRUE
-        )
-      csp <- closestStringProblem(cmatrix, c('a'))
-      paste(csp[1])
-    }
-
-    else if (input$Problema == "Farthest String Problem") {
-      cmatrix <-
-        matrix(
-          data = c('a', 'a', 'a', 'a', 'a', 'a'),
-          ncol = 2,
-          byrow = TRUE
-        )
-      fsp <- farthestStringProblem(cmatrix, c('a'))
-      paste(fsp[1])
-    }
+paste(rvalues$problema[1])
 
   })
   
